d2b3bc6d7e6f4354cad9c90423fb16404a6e169d
Pawel Mazur <mpawel1993@gmail.com>
2022-12-25 21:45:17 +0100

    #1 Initialize repo

---
+++ src/main/java/pl/mazur/pawel/documentsmasterbe/DocumentsMasterBeApplication.java
@@ -0,0 +1,13 @@
+package pl.mazur.pawel.documentsmasterbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsMasterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentsMasterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mazur/pawel/documentsmasterbe/DocumentsMasterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mazur.pawel.documentsmasterbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsMasterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
